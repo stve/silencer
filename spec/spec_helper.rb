a5f1478080d360a00ba2c500375bb63a1e03eac0
Steve Agalloco <steve.agalloco@gmail.com>
2012-07-30 11:26:13 -0400

    don't run simplecov in CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
-require 'simplecov'
+unless ENV['CI']
+  require 'simplecov'
+  SimpleCov.start do
+    add_group 'Silencer', 'lib/silencer'
+    add_group 'Specs', 'spec'
+  end
+end
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
