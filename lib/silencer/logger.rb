7ff520b33c75a32a71b57a30d302d5cf393db44d
Pavel Pravosud <pavel@pravosud.com>
2012-09-19 19:48:33 +0700

    Simplified code a little

--- lib/silencer/logger.rb
+++ lib/silencer/logger.rb
@@ -1,11 +1,11 @@
 require 'rails/rack/logger'
+require 'active_support/core_ext/array/wrap'
 
 module Silencer
   class Logger < Rails::Rack::Logger
     def initialize(app, opts = {})
       @app = app
-      @opts = opts
-      @opts[:silence] ||= []
+      @silence = Array.wrap(opts[:silence])
     end
 
     def call(env)
@@ -21,19 +21,7 @@ module Silencer
     private
 
     def silence_request?(env)
-      silent_header?(env) || silent_request_path?(env) || silent_request_match?(env)
-    end
-
-    def silent_header?(env)
-      env['X-SILENCE-LOGGER']
-    end
-
-    def silent_request_path?(env)
-      @opts[:silence].include?(env['PATH_INFO'])
-    end
-
-    def silent_request_match?(env)
-      @opts[:silence].select { |s| s.kind_of?(Regexp) }.any? { |s| s =~ env['PATH_INFO'] }
+      env['X-SILENCE-LOGGER'] or @silence.any?{ |s| s === env['PATH_INFO'] }
     end
   end
 end
