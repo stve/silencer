daed863d9bd00073e876726494c78b4ecefc7d26
Steve Agalloco <steve.agalloco@gmail.com>
2013-01-30 10:16:14 -0500

    don't adjust load path in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,11 @@
 unless ENV['CI']
   require 'simplecov'
   SimpleCov.start do
+    add_filter '.bundle'
     add_group 'Silencer', 'lib/silencer'
     add_group 'Specs', 'spec'
   end
 end
-
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'rack'
 require 'rails'
