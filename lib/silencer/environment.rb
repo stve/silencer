ba51316dcfa30533093d26e534223f04eba06be3
Steve Agalloco <steve.agalloco@gmail.com>
2018-12-20 15:09:59 -0500

    Remove references to Rails 2 and 3

--- lib/silencer/environment.rb
+++ lib/silencer/environment.rb
@@ -1,8 +1,9 @@
 module Silencer
   module Environment
-    RAILS_2_3 = /^2.3/
-    RAILS_3_2 = /^3.2/
     RAILS_4 = /^4/
+    RAILS_5 = /^5/
+
+    module_function
 
     def rails?
       defined?(::Rails)
@@ -14,23 +15,16 @@ module Silencer
       ::Rails::VERSION::STRING
     end
 
-    def rails2?
-      rails_version =~ RAILS_2_3
-    end
-
-    def rails3_2?
-      rails_version =~ RAILS_3_2
-    end
-
     def rails4?
       rails_version =~ RAILS_4
     end
 
-    def tagged_logger?
-      rails3_2? || rails4?
+    def rails5?
+      rails_version =~ RAILS_5
     end
 
-    module_function :rails?, :rails2?, :rails_version, :rails3_2?
-    module_function :rails4?, :tagged_logger?
+    def tagged_logger?
+      rails4? || rails5?
+    end
   end
 end
